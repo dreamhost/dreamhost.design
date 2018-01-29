e79dfeb2977498a31f2fdf18026fb954e7f24618
Chris Leckie <chris.leckie@dreamhost.com>
2018-01-29 14:44:57 -0800

    make responsive styles for mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,12 @@
 /*
 
-SitePanel Scripts
+Design System Scripts
 
-    This document contains all of the SitePanel-specific front-
+    This document contains all of the design system-specific front-
     end scripts.
 
 */
+
+$( ".menutoggle" ).click(function() {
+    $( "body" ).toggleClass('show');
+});
